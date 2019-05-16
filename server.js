a4937dee03b4b63089d168e05a1105d55ec79570
Roman Alenskiy <kuperman88@gmail.com>
2019-05-17 00:38:18 +0300

    Server adjustment

--- server.js
+++ server.js
@@ -8,24 +8,16 @@ app.get('/api/test', (req, res) => {
   res.send('This test string come from server!')
 })
 
-// Static files
-app.use(express.static(path.join(__dirname, 'client/build')))
-
 // Production mode
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname = 'client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
   })
 }
 
-// Dev mode 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
-})
-
 // Start server
-app.listen(port, (req, res) => {
+app.listen(port, () => {
   console.log(`server listening on port: ${port}`)
 })
