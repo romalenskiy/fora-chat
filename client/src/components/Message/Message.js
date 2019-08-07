7770cbd6d34d3ec2ff9618ed3fb66b32e1ccc3a9
unknown <smolovsky@list.ru>
2019-08-07 20:21:10 +0300

    Stickers v0.1 added

--- client/src/components/Message/Message.js
+++ client/src/components/Message/Message.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Message(props) {
-  const { value, type, username, timestamp } = props
+  const { value, type, username, timestamp, isSticker } = props
 
   const timeString = new Date(timestamp).toTimeString().slice(0, 5)
 
@@ -29,7 +29,9 @@ function Message(props) {
       <div className={messageBalloonClass}>
         {username && <div className="message__username">{username}</div>}
 
-        <div className="message__text">{value}</div>
+        <div className="message__text">{isSticker ? 
+        <div className="message__sticker" style={{backgroundImage: value}}></div>
+        : value}</div>
 
         <div className={messageTimestampClass}>{timeString}</div>
       </div>
---
+++ client/src/components/Stickers/Stickers.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useRef } from 'react'
+import useComponentFocus from '../../customHooks/useComponentFocus'
+import io from 'socket.io-client'
+
+export default function Stickers({getSticker}) {
+    const [stickersDisplay, showStickers] = useState(false);
+
+    const stickersRef = useRef();
+    useComponentFocus(stickersRef, () => { showStickers(false) });
+
+    const changeStickersDisplay = () => {
+        showStickers(!stickersDisplay)
+    }
+
+    const sendSticker = (event) => {
+        showStickers(false);
+        getSticker(event.target.style.backgroundImage);
+    }
+
+    return (
+        <div className="stickers" ref={stickersRef}>
+            <div className="stickers__icon" onClick={changeStickersDisplay}></div>
+            <div className="stickers__container">
+                <div className={stickersDisplay ? 'stickers__block' : 'stickers__block__hide'}>
+                    <div className="stickers__list">
+                        <div className="stickers__item" onClick={sendSticker} style={{backgroundImage: "url(https://img.pngio.com/poo-emoji-sticker-transparent-png-stickpng-sticker-png-517_505.png)"}}></div>
+                        <div className="stickers__item" onClick={sendSticker} style={{backgroundImage: "url(https://img.pngio.com/png-png-the-panda-line-stickers-line-store-sticker-png-240_240.png)"}}></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
