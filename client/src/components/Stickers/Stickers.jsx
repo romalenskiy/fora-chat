9e59fa9c0d2b963c091d04c1f656be929efca4b7
unknown <smolovsky@list.ru>
2019-08-08 16:23:05 +0300

    Stickers v1.0 added

--- client/src/components/Stickers/Stickers.jsx
+++ client/src/components/Stickers/Stickers.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react'
 import useComponentFocus from '../../customHooks/useComponentFocus'
+import stickersArray from './stickersList'
 
 export default function Stickers({getSticker}) {
     const [stickersDisplay, showStickers] = useState(false);
@@ -22,8 +23,7 @@ export default function Stickers({getSticker}) {
             <div className="stickers__container">
                 <div className={stickersDisplay ? 'stickers__block' : 'stickers__block__hide'}>
                     <div className="stickers__list">
-                        <div className="stickers__item" onClick={sendSticker} style={{backgroundImage: "url(https://img.pngio.com/poo-emoji-sticker-transparent-png-stickpng-sticker-png-517_505.png)"}}></div>
-                        <div className="stickers__item" onClick={sendSticker} style={{backgroundImage: "url(https://img.pngio.com/png-png-the-panda-line-stickers-line-store-sticker-png-240_240.png)"}}></div>
+                        {stickersArray.map((sticker)=><div className="stickers__item" onClick={sendSticker} key={sticker.key} title={sticker.key} style={{backgroundImage: sticker.image}}></div>)}
                     </div>
                 </div>
             </div>
---
+++ client/src/components/Stickers/stickersList.js
@@ -0,0 +1,52 @@
+const stickersArray= [
+    {
+        key:'gaben',
+        image:"url(https://www.stickpng.com/assets/thumbs/58f768bf84b713aab61d0a0b.png)"
+    },
+    {
+        key:'grumpy',
+        image:"url(https://www.stickpng.com/assets/thumbs/5845e1677733c3558233c0ee.png)"
+    },
+    {
+        key:'lexa',
+        image:"url(https://www.stickpng.com/assets/thumbs/58967def0803320bf17c2fb6.png)"
+    },
+    {
+        key:'patrick',
+        image:"url(https://www.stickpng.com/assets/thumbs/5cb78f9c7ff3656569c8cec2.png)"
+    },
+    {
+        key:'snoop',
+        image:"url(https://www.stickpng.com/assets/thumbs/58499e0cb89d73775876620b.png)"
+    },
+    {
+        key:'musk',
+        image:"url(https://www.stickpng.com/assets/thumbs/580b57fbd9996e24bc43be26.png)"
+    },
+    {
+        key:'pig',
+        image:"url(https://www.stickpng.com/assets/thumbs/584bcfad2623fb7bc0d05881.png)"
+    },        
+    {
+        key:'pepega',
+        image:"url(https://www.stickpng.com/assets/thumbs/5845cd430b2a3b54fdbaecf8.png)"
+    },
+    {
+        key:'cool',
+        image:"url(https://www.stickpng.com/assets/thumbs/584f0ab16a5ae41a83ddee5f.png)"
+    },
+    {
+        key:'kfc',
+        image:"url(http://assets.stickpng.com/thumbs/590607800cbeef0acff9a644.png)"
+    },
+    {
+        key:'poop',
+        image:"url(https://img.pngio.com/poo-emoji-sticker-transparent-png-stickpng-sticker-png-517_505.png)"
+    },
+    {   
+        key:'panda',
+        image:"url(https://img.pngio.com/png-png-the-panda-line-stickers-line-store-sticker-png-240_240.png)"
+    }
+]
+
+export default stickersArray
